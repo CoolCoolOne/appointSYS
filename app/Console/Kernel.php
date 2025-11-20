11a046d996565d37bc0edf60270b2b6abb6259eb
Troitsky Aleksey <Aleksey-1998@yandex.ru>
2025-11-20 15:23:11 +0300

    исправлены некоторые ошибки важные для сервера

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('slots:delete-past')->dailyAt('00:00');
+        $schedule->command('app:delete-past-slots')->dailyAt('00:00');
     }
 
     /**
--- database/migrations/2025_10_29_134849_create_meetings_table.php
+++ database/migrations/2025_10_29_134849_create_meetings_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             // При удалении ресурса (юнита) удаляем все его слоты. Тут one to many
             $table->foreignId('unit_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
             // Если тайм слот удалён (например слот вчерашнй даты), то пусть информация о встече сохранится. Тут будет one to one. возможно Nullable
-            $table->foreignId('slot_id')->constrained()->onUpdate('cascade')->onDelete('set null');
+            $table->foreignId('slot_id')->nullable()->constrained()->onUpdate('cascade')->onDelete('set null');
             // запрещаю удалять клиента, если он забронировал хоть что то. Тут будет many to one
             $table->foreignId('client_id')->constrained()->onUpdate('cascade')->onDelete('restrict');
             $table->datetime('booked_datetime');
